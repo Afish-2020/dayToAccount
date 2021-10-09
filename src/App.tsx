d84e8cb3dc3d49b70d2cec34275d869a49bbed16
fishyabi <1027096592@qq.com>
2021-10-09 20:20:56 +0800

    导航栏布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import Nav from "components/Nav";
+import React from "react";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from "styled-components";
 
-function App() {
+const Wrapper = styled.div`
+height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+const Main = styled.div`
+flex-grow: 1;
+`
+
+export default function App() {
   return (
-    <div>Hello</div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Redirect exact from="/" to="/money" />
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav />
+      </Wrapper>
+    </Router>
   );
 }
 
-export default App;
+function Tags() {
+  return <h2>Tags</h2>;
+}
+
+function Money() {
+  return <h2>Money</h2>;
+}
+
+function Statistics() {
+  return <h2>Statistics</h2>;
+}
+
+function NoMatch(){
+  return <div>页面不存在</div>
+}
