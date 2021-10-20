cc2dff50d5a519ad29dd0577fceaa69ffc3a4510
fishyabi <1027096592@qq.com>
2021-10-20 21:13:48 +0800

    统计按时间分类

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -1,7 +1,7 @@
 import Layout from "components/Layout";
-import { useRecords } from "Hooks/useRecords";
+import {RecordItem, useRecords } from "Hooks/useRecords";
 import { useTags } from "Hooks/useTags";
-import React, { useState } from "react";
+import React, {ReactNode, useState } from "react";
 import styled from "styled-components";
 import { CategorySection } from "./Money/CategorySection";
 import day from 'dayjs'
@@ -23,10 +23,33 @@ display: flex;
   }
 `
 
+const Header = styled.h3`
+font-size: 18px;
+  line-height: 20px;
+  padding: 10px 16px;
+`
+
 function Statistics() {
   const [catagory,setCatagory] = useState<'-'|'+'>('-')
   const {records} = useRecords()
   const {getName} = useTags()
+  const selectedRecords = records.filter(r=>r.catagory===catagory)
+  const hash:{[k:string]:RecordItem[]} = {}
+  selectedRecords.map(r=>{
+    const key = day(r.createdAt).format('YYYY年MM月DD日')
+    if(!(key in hash)){
+      hash[key] = []
+    }
+    hash[key].push(r)
+  })
+  const array = Object.entries(hash).sort((a,b)=>{
+    if(a[0]===b[0]) return 0
+    if(a[0]>b[0]) return -1
+    if(a[0]<b[0]) return 1
+    return 0
+  })
+  console.log(array);
+
   return (
     <Layout>
       <CategoryWrapper>
@@ -34,16 +57,26 @@ function Statistics() {
       onChange = {value=>setCatagory(value)}
       />
       </CategoryWrapper>
-      {records.map(r=>{
-        return <Item>
-          {r.tagIds.map(tagId=><span>{getName(tagId)}</span>)}
-          {r.note && <div className='note'>
-            {r.note}
-          </div>}
-          {r.amount}
-          {/*{day(r.createdAt).format('YYYY年MM月DD日')}*/}
-        </Item>
-      })}
+      {array.map(([date,records])=>{
+         return <div>
+          <Header>{date}</Header>
+        <div>
+          {records.map(r=>{
+            return <Item>
+              {r.tagIds.map(tagId=><span key={tagId}>{getName(tagId)}</span>)
+                .reduce((result,span,index,array)=>result.concat(
+                  index<array.length-1?[span,'，']:span),[] as ReactNode[])
+              }
+              {r.note && <div className='note'>
+                {r.note}
+              </div>}
+              {r.amount}
+            </Item>
+          })}
+        </div>
+        </div>
+      })
+      }
     </Layout>
   )
 }
