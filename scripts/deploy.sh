01f7dabf53e07c50733d18a031f3134d39131bfa
fishyabi <1027096592@qq.com>
2021-10-21 21:47:42 +0800

    add deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ cd build &&
 git init &&
 git add . &&
 git commit -m "deploy" &&
+git branch -m master main &&
 git remote add origin git@github.com:Afish-2020/dayToAccount-website.git &&
 git push -u origin main -f
 cd -
